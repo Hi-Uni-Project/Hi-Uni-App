39a54d41cadacd1c2a01d33129a1f8a95fd97827
seolbotdae <seolbotdae@gmail.com>
2025-08-03 20:39:01 +0900

    📦️ Chore : 색상 및 폰트 유틸리티 추가

---
+++ src/styles/colors.js
@@ -0,0 +1,23 @@
+module.exports = {
+  system: {
+    'primary-black': '#1F2128',
+    'secondary-purple': '#6568EA',
+    'tertiary-yellow': '#FFF06B',
+    'error-red': '#FF4747',
+  },
+
+  background: {
+    surface: {
+      900: '#1c1c1c',
+      800: '#3c3c3c',
+      700: '#5b5b5b',
+      600: '#6e6e6e',
+      500: '#979797',
+      400: '#b7b7b7',
+      300: '#dadada',
+      200: '#eaeaea',
+      100: '#f3f3f3',
+      50: '#f9f9f9',
+    },
+  },
+};
---
+++ src/styles/fonts.js
@@ -0,0 +1,77 @@
+module.exports = function addFontsUtil({ addUtilities }) {
+  const typoUtilities = {
+    '.typo-title-26-bold': {
+      'font-weight': 700,
+      'font-size': '26px',
+      'line-height': '130%',
+      'letter-spacing': '-2.5%',
+    },
+    '.typo-sub-title-18-medium': {
+      'font-weight': 500,
+      'font-size': '18px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-body-16-bold': {
+      'font-weight': 700,
+      'font-size': '16px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-body-16-regular': {
+      'font-weight': 400,
+      'font-size': '16px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-body-15-regular': {
+      'font-weight': 400,
+      'font-size': '15px',
+      'line-height': '20px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-body-14-semibold': {
+      'font-weight': 600,
+      'font-size': '14px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-body-13-light': {
+      'font-weight': 300,
+      'font-size': '13px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-body-12-light': {
+      'font-weight': 300,
+      'font-size': '12px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-error-14-medium': {
+      'font-weight': 600,
+      'font-size': '14px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-error-14-regular': {
+      'font-weight': 400,
+      'font-size': '14px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-main-button-18-semibold': {
+      'font-weight': 600,
+      'font-size': '18px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+    '.typo-main-button-16-semibold': {
+      'font-weight': 600,
+      'font-size': '16px',
+      'line-height': '24px',
+      'letter-spacing': '-2%',
+    },
+  };
+  addUtilities({ ...typoUtilities });
+};
