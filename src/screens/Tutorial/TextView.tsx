95c435aca8bf8b6e41ee724623682532c111d283
seolbotdae <seolbotdae@gmail.com>
2025-08-15 18:17:31 +0900

    ✨ Feat :  텍스트 표시 애니메이션 적용

--- src/screens/Tutorial/TextView.tsx
+++ src/screens/Tutorial/TextView.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import Animated, { FadeOutUp, FadeInDown } from 'react-native-reanimated';
 
 import TextProvider from './TextProvider';
 
 const TextView = ({ currentStep }: { currentStep: number }) => {
   const textProvider = TextProvider;
   return (
-    <View className="flex-1">
+    <View key={`TextView-${currentStep}`} className="flex-1">
       <View className="m-5 mt-28">
-        <Text className="text-center typo-title-26-bold">
-          {textProvider[currentStep].title}
-        </Text>
-        <Text className="mt-2 text-center text-gray-600 typo-sub-title-18-medium">
-          {textProvider[currentStep].subTitle}
-        </Text>
+        {textProvider[currentStep].title.split('\n').map((line, index) => (
+          <Animated.Text
+            key={`TextView-${currentStep}-Title-${index}`}
+            entering={FadeInDown.delay(300).duration(400 + index * 80)}
+            exiting={FadeOutUp.duration(500)}
+            className="text-center typo-title-26-bold">
+            {line}
+          </Animated.Text>
+        ))}
+
+        <View className="mb-4" />
+
+        {textProvider[currentStep].subTitle.split('\n').map((line, index) => (
+          <Animated.Text
+            key={`TextView-${currentStep}-subTitle-${index}`}
+            entering={FadeInDown.delay(300).duration(600 + index * 80)}
+            exiting={FadeOutUp.duration(500)}
+            className="text-center text-gray-600 typo-sub-title-18-medium">
+            {line}
+          </Animated.Text>
+        ))}
       </View>
     </View>
   );
--- src/screens/Tutorial/index.tsx
+++ src/screens/Tutorial/index.tsx
@@ -13,8 +13,15 @@ const TutorialScreen = () => {
   const progress = useSharedValue<number>(0);
   const textProvider = TextProvider;
 
-  const onPressPagination = (index: number) => {
-    progress.value = withSpring(index);
+  const movePageByIndex = (index: number) => {
+    const length = textProvider.length;
+
+    if (index < length) {
+      setCurrentStep(index);
+      progress.value = withSpring(index);
+    } else {
+      // navigating 및 온보딩 여부 체크가 들어갑니다.
+    }
   };
 
   return (
@@ -23,19 +30,22 @@ const TutorialScreen = () => {
       <ButtonView
         currentStep={currentStep}
         onNext={() => {
-          setCurrentStep(currentStep + 1);
-          onPressPagination(currentStep + 1);
+          movePageByIndex(currentStep + 1);
         }}
       />
       <View className="relative w-full items-center justify-center py-5">
         <PaginationView progress={progress} data={textProvider} />
         <Pressable
-          className="absolute right-3"
+          className="absolute right-5"
           onPress={() => {
-            setCurrentStep(0);
-            onPressPagination(0);
+            // navigating 들어갑니다.
+
+            // 개발 단계에서는 임시로 첫 번째 페이지로 이동하는 코드 넣어두겠습니다.
+            if (process.env.NODE_ENV === 'development') {
+              movePageByIndex(0);
+            }
           }}>
-          <Text>건너뛰기</Text>
+          <Text className="text-gray-500">건너뛰기</Text>
         </Pressable>
       </View>
     </SafeAreaView>
