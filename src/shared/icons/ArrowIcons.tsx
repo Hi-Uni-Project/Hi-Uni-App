3bbf50d6aa708e82a9651de919b8159dacd02c5a
hyeone999 <caps026@naver.com>
2025-09-01 01:42:35 +0900

    ✨ Feat :  arrow 아이콘 추가

--- src/shared/icons/ArrowIcons.tsx
+++ src/shared/icons/ArrowIcons.tsx
@@ -3,9 +3,10 @@ import * as React from 'react';
 import { ColorValue } from 'react-native';
 
 import ArrowLeft from '@/static/icons/left_arrow.svg';
+import ArrowRight from '@/static/icons/right_arrow.svg';
 
 interface Props {
-  direction: 'left';
+  direction: 'left' | 'right';
   color?: ColorValue;
   width?: number;
   height?: number;
@@ -15,6 +16,8 @@ const ArrowIcons = ({ direction, color, width, height }: Props) => {
   switch (direction) {
     case 'left':
       return <ArrowLeft color={color} width={width} height={height} />;
+    case 'right':
+      return <ArrowRight color={color} width={width} height={height} />;
     default:
       return null;
   }
