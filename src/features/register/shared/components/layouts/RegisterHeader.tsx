fa9c7e107b3d8a1a941e3f661344008b87c89d62
hyeone999 <caps026@naver.com>
2025-09-01 16:44:32 +0900

    ✨ Feat :  회원가입 플로우 상단 헤더 구현

--- src/features/register/shared/components/layouts/RegisterHeader.tsx
+++ src/features/register/shared/components/layouts/RegisterHeader.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
-interface Props {
+interface RegisterHeaderProps {
   main: string;
   sub: string;
+  variant?: 'default' | 'code';
 }
 
-const RegisterHeader = ({ main, sub }: Props) => {
+const RegisterHeader = ({
+  main,
+  sub,
+  variant = 'default',
+}: RegisterHeaderProps) => {
+  if (variant === 'code') {
+    return (
+      <View className="mt-8 items-center space-y-2 px-8">
+        <Text className="text-main-text typo-title-26-bold">{main}</Text>
+        <Text className="text-center text-surface-600 typo-body-16-regular">
+          {sub}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View className="space-y-1 pl-[30px]">
       <Text className="text-surface-600 typo-sub-title-18-medium">{main}</Text>
-      <Text className="text-main-text typo-title-26-bold">{sub} </Text>
+      <Text className="text-main-text typo-title-26-bold">{sub}</Text>
     </View>
   );
 };
