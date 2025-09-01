a5a061cdeb5f025f2180fb279d618a7d2d9c109e
hyeone999 <caps026@naver.com>
2025-09-01 17:41:10 +0900

    ✨ Feat :  컴포넌트 path 수정 반영

--- src/screens/Register/MailInput.tsx
+++ src/screens/Register/MailInput.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { TextInput, View } from 'react-native';
 
-import ErrorTextArea from '@/features/register/inputMail/components/errorTextArea';
+import ErrorTextArea from '@/features/register/inputMail/components/ErrorTextArea';
 import RegisterHeader from '@/features/register/shared/components/layouts/RegisterHeader';
 import KeyboardAvoidingLayout from '@/shared/components/layouts/KeyboardAvoidingLayout';
 import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
