7e89ec7dc817b24672f88bc21f9954a7e489c5a6
hyeone999 <caps026@naver.com>
2025-09-01 17:39:46 +0900

    ✨ Feat :  회원가입 환영 페이지 구현

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import WelcomeTextBox from '@/features/welcome/components/WelcomeTextBox';
+import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
+import HUButton from '@/shared/ui/atoms/HUButton';
+
+const Welcome = () => {
+  return (
+    <ScreenLayout className="items-center bg-primary-purple">
+      <WelcomeTextBox
+        main="환영합니다!"
+        sub={`하이유니와 함께
+커리어 성장하실 준비되셨나요?`}
+      />
+
+      <HUButton variant="black" text="커리어 성장하러가기" />
+    </ScreenLayout>
+  );
+};
+
+export default Welcome;
