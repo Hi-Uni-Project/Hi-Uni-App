834fd200066f8e9a724f3643bf9818d5aba24ca3
hyeone999 <caps026@naver.com>
2025-09-03 19:19:08 +0900

    ✨ Feat :  header layout 적용

--- src/screens/Register/MailCodeInput.tsx
+++ src/screens/Register/MailCodeInput.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import MailCodeErrorText from '@/features/register/mailCode/components/MailCodeErrorText';
 import MailCodeField from '@/features/register/mailCode/components/MailCodeField';
 import useMailCode from '@/features/register/mailCode/hooks/useMailCode';
+import RegisterDefaultLayout from '@/features/register/shared/components/layouts/RegisterDefaultLayout';
 import RegisterHeader from '@/features/register/shared/components/layouts/RegisterHeader';
 import { CELL_COUNT } from '@/features/register/shared/constants/codeCell';
+import HeaderWithBack from '@/shared/components/layouts/HeaderWithBack';
 import KeyboardAvoidingLayout from '@/shared/components/layouts/KeyboardAvoidingLayout';
 import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
 import HUButton from '@/shared/ui/atoms/HUButton';
@@ -16,22 +18,26 @@ const MailCodeInput = () => {
   return (
     <KeyboardAvoidingLayout>
       <ScreenLayout>
-        <RegisterHeader
-          variant="code"
-          main="인증번호를 입력해주세요."
-          sub={`20200525@knu.ac.kr로
+        <HeaderWithBack flow screen="3" />
+
+        <RegisterDefaultLayout>
+          <RegisterHeader
+            variant="code"
+            main="인증번호를 입력해주세요."
+            sub={`20200525@knu.ac.kr로
 인증번호를 발송하였습니다.`}
-        />
+          />
 
-        <MailCodeField
-          codeFieldRef={codeFieldRef}
-          value={value}
-          onChangeText={setValue}
-          props={props}
-          getCellOnLayoutHandler={getCellOnLayoutHandler}
-        />
+          <MailCodeField
+            codeFieldRef={codeFieldRef}
+            value={value}
+            onChangeText={setValue}
+            props={props}
+            getCellOnLayoutHandler={getCellOnLayoutHandler}
+          />
 
-        <MailCodeErrorText />
+          <MailCodeErrorText />
+        </RegisterDefaultLayout>
       </ScreenLayout>
 
       <HUButton
