20fd8f74cef8979714fbf61ca9443d36164a3e85
seolbotdae <seolbotdae@gmail.com>
2025-09-05 18:48:28 +0900

    ✨ Feat :  chevron 오른쪽 추가

--- src/shared/icons/ChevronIcons.tsx
+++ src/shared/icons/ChevronIcons.tsx
@@ -4,10 +4,11 @@ import { ColorValue } from 'react-native';
 
 import ChevronBottom from '@/static/icons/down_chevron.svg';
 import ChevronLeft from '@/static/icons/left_chevron.svg';
+import ChevronRight from '@/static/icons/right_chevron.svg';
 import ChevronTop from '@/static/icons/top_chevron.svg';
 
 interface Props {
-  direction: 'left' | 'top' | 'bottom';
+  direction: 'left' | 'top' | 'bottom' | 'right';
   color?: ColorValue;
   width?: number;
   height?: number;
@@ -17,6 +18,8 @@ const ChevronIcons = ({ direction, color, width, height }: Props) => {
   switch (direction) {
     case 'left':
       return <ChevronLeft color={color} width={width} height={height} />;
+    case 'right':
+      return <ChevronRight color={color} width={width} height={height} />;
     case 'top':
       return <ChevronTop color={color} width={width} height={height} />;
     case 'bottom':
