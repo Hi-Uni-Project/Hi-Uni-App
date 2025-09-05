1c955e68861bd4637144804abe933e007295ba34
seolbotdae <seolbotdae@gmail.com>
2025-09-05 18:49:52 +0900

    ✨ Feat :  안드로이드와 ios 동시 동작하는 shadow style 추가

---
+++ src/styles/shadowStyleSheet.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const shadowStyleSheet = StyleSheet.create({
+  dropShadow: {
+    boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.03)',
+  },
+});
+
+export default shadowStyleSheet;
