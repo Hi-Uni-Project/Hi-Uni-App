1921d07d0cb0bc82e18774af0764a74fc488c1d8
seolbotdae <seolbotdae@gmail.com>
2025-09-06 11:29:17 +0900

    ➖ Remove :  shadow 스타일시트 삭제

--- src/styles/shadowStyleSheet.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const shadowStyleSheet = StyleSheet.create({
-  dropShadow: {
-    boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.03)',
-  },
-});
-
-export default shadowStyleSheet;
