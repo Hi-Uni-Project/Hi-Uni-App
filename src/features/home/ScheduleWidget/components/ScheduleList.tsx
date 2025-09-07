da07d3ae1affc27335189d0def8f91da377e81a7
seolbotdae <seolbotdae@gmail.com>
2025-09-07 15:23:20 +0900

    ✨ Feat :  일정 추가하기 버튼 추가

--- src/features/home/ScheduleWidget/components/ScheduleList.tsx
+++ src/features/home/ScheduleWidget/components/ScheduleList.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Pressable } from 'react-native';
 import Animated, { FadeIn } from 'react-native-reanimated';
 
 import { MockScheduleData } from '../mocks/scheduleMock';
 import formatKoreanDate from '../utils/formatKoreanDate';
 
 import ScheduleListItem from './ScheduleListItem';
+
+import AddIcons from '@/static/icons/add.svg';
 
 const ItemSeparator = () => <View className="h-[6px]" />;
 
@@ -31,7 +33,7 @@ const ScheduleList = ({ selectedDate, schedule }: ScheduleListProps) => {
     : [];
 
   return (
-    <View>
+    <View className="relative">
       <Text className="mb-[15px] text-[15px] font-normal">
         {formatKoreanDate(selectedDate)}
       </Text>
@@ -49,6 +51,17 @@ const ScheduleList = ({ selectedDate, schedule }: ScheduleListProps) => {
         ItemSeparatorComponent={ItemSeparator}
         ListEmptyComponent={ListEmptyComponent}
       />
+      {scheduleItems.length === 0 && (
+        <Pressable
+          className="absolute right-0 top-0"
+          onPress={() => {
+            console.log('일정 추가하기');
+          }}>
+          <Animated.View entering={FadeIn.delay(150)}>
+            <AddIcons width={20} height={20} color={'#B7B7B7'} />
+          </Animated.View>
+        </Pressable>
+      )}
     </View>
   );
 };
