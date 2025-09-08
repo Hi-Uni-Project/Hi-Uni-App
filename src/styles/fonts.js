8d5d66914816bbc4d8e85b3763ec50cd36f5786b
seolbotdae <seolbotdae@gmail.com>
2025-09-08 19:38:42 +0900

    ✨ Feat :  신규 폰트 추가

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,5 +1,6 @@
 module.exports = function addFontsUtil({ addUtilities }) {
   const typoUtilities = {
+    // title
     '.typo-title-28-bold': {
       fontWeight: 700,
       fontSize: 28,
@@ -12,17 +13,31 @@ module.exports = function addFontsUtil({ addUtilities }) {
       lineHeight: '33.8px',
       letterSpacing: -0.65,
     },
-    '.typo-title-22-semibold': {
+
+    // sub title
+    '.typo-sub-title-22-bold': {
+      fontWeight: 700,
+      fontSize: 22,
+      lineHeight: '24px',
+      letterSpacing: -0.55,
+    },
+    '.typo-sub-title-22-semibold': {
       fontWeight: 600,
       fontSize: 22,
       lineHeight: '24px',
-      letterSpacing: -0.28,
+      letterSpacing: -0.55,
     },
-    '.typo-main-button-18-semibold': {
+    '.typo-sub-title-20-semibold': {
       fontWeight: 600,
-      fontSize: 18,
+      fontSize: 20,
       lineHeight: '24px',
-      letterSpacing: -0.36,
+      letterSpacing: -0.5,
+    },
+    '.typo-sub-title-20-medium': {
+      fontWeight: 500,
+      fontSize: 20,
+      lineHeight: '24px',
+      letterSpacing: -0.5,
     },
     '.typo-sub-title-18-medium': {
       fontWeight: 500,
@@ -30,20 +45,16 @@ module.exports = function addFontsUtil({ addUtilities }) {
       lineHeight: '24px',
       letterSpacing: -0.36,
     },
-    '.typo-sub-title-18-regular': {
-      fontWeight: 400,
-      fontSize: 18,
-      lineHeight: '24px',
-      letterSpacing: -0.36,
-    },
-    '.typo-sub-title-17-semibold': {
-      fontWeight: 500,
+
+    // body
+    '.typo-body-17-semibold': {
+      fontWeight: 600,
       fontSize: 17,
       lineHeight: '24px',
       letterSpacing: -0.34,
     },
-    '.typo-sub-title-17-medium': {
-      fontWeight: 600,
+    '.typo-body-17-medium': {
+      fontWeight: 500,
       fontSize: 17,
       lineHeight: '24px',
       letterSpacing: -0.34,
@@ -54,8 +65,14 @@ module.exports = function addFontsUtil({ addUtilities }) {
       lineHeight: '24px',
       letterSpacing: -0.32,
     },
-    '.typo-main-button-16-semibold': {
+    '.typo-body-16-semibold': {
       fontWeight: 600,
+      fontSize: 16,
+      lineHeight: '24px',
+      letterSpacing: -0.32,
+    },
+    '.typo-body-16-medium': {
+      fontWeight: 500,
       fontSize: 16,
       lineHeight: '24px',
       letterSpacing: -0.32,
@@ -66,62 +83,181 @@ module.exports = function addFontsUtil({ addUtilities }) {
       lineHeight: '24px',
       letterSpacing: -0.32,
     },
+    '.typo-body-15-semibold': {
+      fontWeight: 600,
+      fontSize: 15,
+      lineHeight: '20px',
+      letterSpacing: -0.3,
+    },
+    '.typo-body-15-medium': {
+      fontWeight: 500,
+      fontSize: 15,
+      lineHeight: '20px',
+      letterSpacing: -0.3,
+    },
     '.typo-body-15-regular': {
       fontWeight: 400,
       fontSize: 15,
       lineHeight: '20px',
       letterSpacing: -0.3,
     },
-    '.typo-body-15-normal': {
-      fontWeight: 500,
-      fontSize: 15,
+
+    // caption
+    '.typo-caption-14-semibold': {
+      fontWeight: 600,
+      fontSize: 14,
+      lineHeight: '24px',
+      letterSpacing: -0.28,
+    },
+    '.typo-caption-14-regular': {
+      fontWeight: 400,
+      fontSize: 14,
       lineHeight: '20px',
-      letterSpacing: -0.3,
-    },
-    '.typo-body-14-semibold': {
-      fontWeight: 600,
-      fontSize: 14,
-      lineHeight: '24px',
       letterSpacing: -0.28,
     },
-    '.typo-14-regular': {
-      fontWeight: 400,
+    '.typo-caption-14-light': {
+      fontWeight: 300,
       fontSize: 14,
-      lineHeight: '24px',
+      lineHeight: '20px',
       letterSpacing: -0.28,
     },
-    '.typo-error-14-medium': {
-      color: '#FB6C6C',
-      fontWeight: 600,
-      fontSize: 14,
+
+    '.typo-caption-13-medium': {
+      fontWeight: 500,
+      fontSize: 13,
       lineHeight: '24px',
-      letterSpacing: -0.28,
+      letterSpacing: -0.26,
     },
-    '.typo-error-14-regular': {
-      color: '#FB6C6C',
-      fontWeight: 400,
-      fontSize: 14,
-      lineHeight: '24px',
-      letterSpacing: -0.28,
-    },
-    '.typo-body-13-light': {
+    '.typo-caption-13-light': {
       fontWeight: 300,
       fontSize: 13,
       lineHeight: '24px',
       letterSpacing: -0.26,
     },
-    '.typo-bottom-bar-13-medium': {
-      fontWeight: 500,
-      fontSize: 13,
-      lineHeight: '24px',
-      letterSpacing: -0.26,
-    },
-    '.typo-body-12-light': {
+
+    '.typo-caption-12-light': {
       fontWeight: 300,
       fontSize: 12,
       lineHeight: '24px',
       letterSpacing: -0.24,
     },
+
+    '.typo-caption-11-medium': {
+      fontWeight: 500,
+      fontSize: 11,
+      lineHeight: '24px',
+      letterSpacing: -0.22,
+    },
+
+    // error
+    '.typo-error-14-medium': {
+      color: '#FB6C6C',
+      fontWeight: 500,
+      fontSize: 14,
+      lineHeight: '24px',
+      letterSpacing: -0.28,
+    },
+
+    // main button
+    '.typo-main-button-18-semibold': {
+      fontWeight: 600,
+      fontSize: 18,
+      lineHeight: '24px',
+      letterSpacing: 0,
+    },
+
+    // sub button
+    '.typo-sub-button-16-semibold': {
+      fontWeight: 600,
+      fontSize: 16,
+      lineHeight: '24px',
+      letterSpacing: 0,
+    },
   };
   addUtilities({ ...typoUtilities });
 };
+
+// '.typo-title-22-semibold': {
+//   fontWeight: 600,
+//   fontSize: 22,
+//   lineHeight: '24px',
+//   letterSpacing: -0.28,
+// },
+
+// '.typo-sub-title-18-regular': {
+//   fontWeight: 400,
+//   fontSize: 18,
+//   lineHeight: '24px',
+//   letterSpacing: -0.36,
+// },
+
+// '.typo-sub-title-17-semibold': {
+//   fontWeight: 500,
+//   fontSize: 17,
+//   lineHeight: '24px',
+//   letterSpacing: -0.34,
+// },
+
+// '.typo-sub-title-17-medium': {
+//   fontWeight: 600,
+//   fontSize: 17,
+//   lineHeight: '24px',
+//   letterSpacing: -0.34,
+// },
+
+// '.typo-body-15-normal': {
+//   fontWeight: 500,
+//   fontSize: 15,
+//   lineHeight: '20px',
+//   letterSpacing: -0.3,
+// },
+
+// '.typo-body-14-semibold': {
+//   fontWeight: 600,
+//   fontSize: 14,
+//   lineHeight: '24px',
+//   letterSpacing: -0.28,
+// },
+
+// '.typo-main-button-16-semibold': {
+//   fontWeight: 600,
+//   fontSize: 16,
+//   lineHeight: '24px',
+//   letterSpacing: -0.32,
+// },
+
+// '.typo-14-regular': {
+//   fontWeight: 400,
+//   fontSize: 14,
+//   lineHeight: '24px',
+//   letterSpacing: -0.28,
+// },
+
+// '.typo-error-14-regular': {
+//   color: '#FB6C6C',
+//   fontWeight: 400,
+//   fontSize: 14,
+//   lineHeight: '24px',
+//   letterSpacing: -0.28,
+// },
+
+// '.typo-body-13-light': {
+//   fontWeight: 300,
+//   fontSize: 13,
+//   lineHeight: '24px',
+//   letterSpacing: -0.26,
+// },
+
+// '.typo-bottom-bar-13-medium': {
+//   fontWeight: 500,
+//   fontSize: 13,
+//   lineHeight: '24px',
+//   letterSpacing: -0.26,
+// },
+
+// '.typo-body-12-light': {
+//   fontWeight: 300,
+//   fontSize: 12,
+//   lineHeight: '24px',
+//   letterSpacing: -0.24,
+// },
