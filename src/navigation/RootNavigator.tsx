38f609df253320732a0e45516db34bceb321e315
seolbotdae <seolbotdae@gmail.com>
2025-09-09 17:36:35 +0900

    📦️ Chore :  navigation 추가

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import HomeScreen from '@/screens/Home';
 import LoginScreen from '@/screens/Login';
 import OnboardingScreen from '@/screens/Onboarding';
 import TermsScreen from '@/screens/Terms';
@@ -11,11 +12,12 @@ const Stack = createNativeStackNavigator();
 const RootStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Onboarding"
+      initialRouteName="Home"
       screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Term" component={TermsScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
