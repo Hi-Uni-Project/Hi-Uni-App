fbb15e585fdf63dd56377fd24dff3eb1c54cc4ec
seolbotdae <seolbotdae@gmail.com>
2025-09-13 14:18:55 +0900

    ✨ Feat :  헤더 구현 후 추가

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { ScrollView } from 'react-native';
 import Animated, { LinearTransition } from 'react-native-reanimated';
 
 import DynamicCTABanner from '@/features/home/dynamicCTABanner/components/DynamicCTABanner';
@@ -8,35 +7,30 @@ import NextUpdateTemplate from '@/features/home/nextUpdateTemplate/components/Ne
 import ScheduleWidget from '@/features/home/scheduleWidget/components/ScheduleWidget';
 import UnivMajorBadge from '@/features/home/univMajorBadge/components/UnivMajorBadge';
 import HotPostsWidget from '@/features/home/weeklyHotPosts/components/HotPostsWidget';
-import HomeLayout from '@/shared/components/layouts/HomeLayout';
+import HomeScrollLayout from '@/shared/components/layouts/HomeScrollLayout';
 
 const HomeScreen = () => {
   return (
-    <HomeLayout className="bg-surface-50">
-      <ScrollView>
-        <UnivMajorBadge
-          univName="제주대학교"
-          majorName="산업디자인학과 20학번"
-        />
-        <DynamicCTABanner
-          visible
-          onSharedPress={() => {
-            console.log('공유 페이지 네비게이션');
-          }}
-        />
-        <ScheduleWidget />
-        <HotPostsWidget />
-        <Animated.View
-          style={{ rowGap: 38, marginTop: 38 }}
-          layout={LinearTransition}>
-          <NextUpdateTemplate title="대외활동" />
+    <HomeScrollLayout>
+      <UnivMajorBadge univName="제주대학교" majorName="산업디자인학과 20학번" />
+      <DynamicCTABanner
+        visible
+        onSharedPress={() => {
+          console.log('공유 페이지 네비게이션');
+        }}
+      />
+      <ScheduleWidget />
+      <HotPostsWidget />
+      <Animated.View
+        style={{ rowGap: 38, marginTop: 38 }}
+        layout={LinearTransition}>
+        <NextUpdateTemplate title="대외활동" />
 
-          <NextUpdateTemplate title="인턴십" />
+        <NextUpdateTemplate title="인턴십" />
 
-          <NextUpdateTemplate title="채용공고" />
-        </Animated.View>
-      </ScrollView>
-    </HomeLayout>
+        <NextUpdateTemplate title="채용공고" />
+      </Animated.View>
+    </HomeScrollLayout>
   );
 };
 
