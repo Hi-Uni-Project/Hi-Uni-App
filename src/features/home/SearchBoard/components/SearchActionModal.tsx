cb4f8510e7a098ad3546ab7a86b5ec199dbd315c
hyeone999 <caps026@naver.com>
2025-09-17 21:59:16 +0900

    ✨ Feat :  홈-최근검색어 action에 따른 modal 구현

---
+++ src/features/home/SearchBoard/components/SearchActionModal.tsx
@@ -0,0 +1,43 @@
+import React, { Dispatch, SetStateAction } from 'react';
+
+import SortBottomSheet from './SortBottomSheet';
+
+import ConfirmModal from '@/shared/ui/organisms/ConfirmModal';
+
+interface Props {
+  modalVisible: boolean;
+  onModalConfirm: () => void;
+  selectedSort: string;
+  setSelectedSort: Dispatch<SetStateAction<string>>;
+  sortSheetVisible: boolean;
+  setSortSheetVisible: Dispatch<SetStateAction<boolean>>;
+}
+
+const SearchActionModal = ({
+  modalVisible,
+  onModalConfirm,
+  selectedSort,
+  setSelectedSort,
+  sortSheetVisible,
+  setSortSheetVisible,
+}: Props) => {
+  return (
+    <>
+      <ConfirmModal
+        confirmText="네, 확인했어요"
+        title="두 글자 이상 입력해주세요."
+        visible={modalVisible}
+        onConfirm={onModalConfirm}
+      />
+
+      <SortBottomSheet
+        selectedSort={selectedSort}
+        setSelectedSort={setSelectedSort}
+        sortSheetVisible={sortSheetVisible}
+        setSortSheetVisible={setSortSheetVisible}
+      />
+    </>
+  );
+};
+
+export default SearchActionModal;
