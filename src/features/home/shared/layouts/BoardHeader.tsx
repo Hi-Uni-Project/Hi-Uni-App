a9057f7303adccc565fa3aca4bc2a5f9a632c127
hyeone999 <caps026@naver.com>
2025-09-17 22:00:11 +0900

    ✨ Feat :  홈화면에서 전역적으로 사용할 수 있는 헤더 컴포넌트 레이아웃 구현

---
+++ src/features/home/shared/layouts/BoardHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { SafeAreaView } from 'react-native';
+
+const BoardHeaderColor = () => {
+  return <SafeAreaView style={{ backgroundColor: 'white' }} />;
+};
+
+export default BoardHeaderColor;
---
+++ src/features/home/shared/layouts/BoardSectionHeader.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { Pressable, Text, View } from 'react-native';
+
+import Filtered from '@/static/icons/filtered.svg';
+
+interface Props {
+  hasSearchResults: boolean;
+  hasRecentSearches: boolean;
+  hasSearched: boolean;
+  selectedSort: string;
+  onClearAll: () => void;
+}
+
+const BoardSectionHeader = ({
+  hasSearchResults,
+  hasRecentSearches,
+  hasSearched,
+  selectedSort,
+  onClearAll,
+}: Props) => {
+  return (
+    <View className="mb-2 flex-row items-center justify-between">
+      {hasSearchResults ? (
+        <Pressable className="flex-row items-center gap-1.5">
+          <Filtered />
+          <Text className="text-surface-500 typo-body-16-regular">
+            {selectedSort}
+          </Text>
+        </Pressable>
+      ) : (
+        !hasSearched && (
+          <Text className="text-main-text typo-sub-title-20-semibold">
+            최근 검색어
+          </Text>
+        )
+      )}
+
+      {hasRecentSearches && !hasSearched && (
+        <Pressable onPress={onClearAll}>
+          <Text className="text-surface-500 typo-body-16-medium">전체삭제</Text>
+        </Pressable>
+      )}
+    </View>
+  );
+};
+
+export default BoardSectionHeader;
