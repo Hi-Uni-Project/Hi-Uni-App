fb04f714c0170389bbe85d28b87246609ce8a1bf
hyeone999 <caps026@naver.com>
2025-09-22 13:31:27 +0900

    ✨ Feat :  kakao-SDK initialize 함수 구현

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { initializeKakaoSDK } from '@react-native-kakao/core';
+import { AppRegistry } from 'react-native';
+import Config from 'react-native-config';
+
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+initializeKakaoSDK(Config.KAKAO_APP_KEY);
 
 AppRegistry.registerComponent(appName, () => App);
