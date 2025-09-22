d918c52e0ba1549d5f12c099c502ca791873c70c
hyeone999 <caps026@naver.com>
2025-09-22 13:58:09 +0900

    ✨ Feat :  naver-login api 구현

---
+++ src/features/login/lib/socialStrategies/naver.ts
@@ -0,0 +1,7 @@
+import NaverLogin from '@react-native-seoul/naver-login';
+
+export const naverLogin = async (): Promise<string> => {
+  const response = await NaverLogin.login();
+
+  return response.successResponse.accessToken;
+};
