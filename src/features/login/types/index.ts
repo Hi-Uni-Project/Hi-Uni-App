5e303bc7cdc2df026528d952974aca6408e01924
hyeone999 <caps026@naver.com>
2025-09-23 11:20:45 +0900

    ✨ Feat :  login 관련 type 선언

---
+++ src/features/login/types/index.ts
@@ -0,0 +1,19 @@
+export type SocialTypes = 'kakao' | 'naver' | 'google' | 'apple';
+export type SocialLoginFunction = () => Promise<string>;
+
+export interface LoginRequest {
+  provider: SocialTypes;
+  authToken: string;
+}
+
+export interface LoginResponse {
+  localDateTime: string;
+  responseCode: number;
+  statusCode: string;
+  message: string;
+  data: {
+    accessToken: string;
+    refreshToken: string;
+    isSignUp: boolean;
+  };
+}
