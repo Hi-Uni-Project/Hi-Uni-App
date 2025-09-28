f3b5f3ee55bfb9d90d225588a2f9de0942a7d875
hyeone999 <caps026@naver.com>
2025-09-28 12:47:16 +0900

    ✨ Feat :  분리된 서비스 로직으로 수정하여 반영

--- src/features/login/ui/organisms/SocialLoginSection.tsx
+++ src/features/login/ui/organisms/SocialLoginSection.tsx
@@ -1,42 +1,17 @@
 import React from 'react';
 
+import { useNavigation } from '@react-navigation/native';
 import { View } from 'react-native';
 
-import { loginApi } from '../../api/loginApi';
-import { loginStrategies } from '../../lib/socialStrategies';
-import { LoginRequest, SocialTypes } from '../../types';
+import { useLoginService } from '../../hooks/useUserLogin';
 
+import { MainStackNavigationProp } from '@/navigation/types/navigationTypes';
 import { SOCIAL_PROVIDERS } from '@/shared/constants/socialProvider';
 import HUButton from '@/shared/ui/atoms/HUButton';
 
 const SocialLoginSection = () => {
-  // 유저 세션 비즈니스 로직 구현하면서 서비스 함수 한번에 정리 예정입니다.
-  // 이번 PR 에서는 러프하게 봐주셔도 되는 코드들입니다.
-  const handleSocialLogin = async (provider: SocialTypes) => {
-    try {
-      const authToken = await loginStrategies[provider]();
-
-      // setSocialAccessToken(socialAccessToken);
-
-      // handleLogin({ socialType, socialAccessToken });
-
-      console.log(authToken);
-
-      handleLogin({ authToken, provider });
-    } catch (err) {
-      console.error(`${provider} 로그인 실패:`, err);
-    }
-  };
-
-  const handleLogin = async (loginPayload: LoginRequest) => {
-    try {
-      const response = await loginApi(loginPayload);
-
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const navigation = useNavigation<MainStackNavigationProp>();
+  const { handleSocialLogin } = useLoginService(navigation);
 
   return (
     <View className="w-full items-center gap-[7px] pb-11">
