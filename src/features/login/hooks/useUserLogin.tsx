caac15c250e7ecd55a2e4a33a2071fdb325e909a
hyeone999 <caps026@naver.com>
2025-09-28 12:47:38 +0900

    ✨ Feat :  유저 로그인 서비스 로직 구현

---
+++ src/features/login/hooks/useUserLogin.tsx
@@ -0,0 +1,45 @@
+import { loginApi } from '../api/loginApi';
+import { loginStrategies } from '../lib/socialStrategies';
+import { LoginRequest, LoginResponse, SocialTypes } from '../types';
+
+import { MainStackNavigationProp } from '@/navigation/types/navigationTypes';
+import { useUserStore } from '@/shared/stores/user';
+
+export const useLoginService = (navigation: MainStackNavigationProp) => {
+  const { setAccessToken, setRefreshToken } = useUserStore();
+
+  const handleSocialLogin = async (provider: SocialTypes) => {
+    try {
+      const authToken = await loginStrategies[provider]();
+
+      handleLogin({ authToken, provider });
+    } catch (err) {
+      console.error(`${provider} 로그인 실패:`, err);
+    }
+  };
+
+  const handleLogin = async (loginPayload: LoginRequest) => {
+    try {
+      const response = await loginApi(loginPayload);
+
+      if (response.data.isSignUp) {
+        handleSuccessfulLogin(response);
+      } else {
+        navigation.navigate('SignupRoute');
+      }
+    } catch (err) {
+      console.error('로그인 실패:', err);
+    }
+  };
+
+  const handleSuccessfulLogin = (response: LoginResponse) => {
+    setAccessToken(response.data.accessToken);
+    setRefreshToken(response.data.refreshToken);
+
+    navigation.navigate('HomeRoute');
+  };
+
+  return {
+    handleSocialLogin,
+  };
+};
