493fcd5f50eb911da5493dd7d45a9a10c88b12be
hyeone999 <caps026@naver.com>
2025-09-29 10:21:17 +0900

    ✨ Feat :  토큰 재발급 로직 type 선언

--- src/shared/api/types/index.ts
+++ src/shared/api/types/index.ts
@@ -1,7 +1,11 @@
-export interface ResponseType<T = unknown> {
+export interface ResponseTypes<T = unknown> {
   data: T;
   localDateTime: string;
   message: string;
   responseCode: number;
   statusCode: string;
+}
+
+export interface Refresh {
+  accessToken: string;
 }
