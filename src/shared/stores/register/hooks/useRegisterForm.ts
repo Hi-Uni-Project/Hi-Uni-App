a3266a469f474e0dbe5bfe22f58bd28411ca70c6
hyeone999 <caps026@naver.com>
2025-09-30 14:47:57 +0900

    ✨ Feat :  회원가입 form을 위한 hooks 구현

---
+++ src/shared/stores/register/hooks/useRegisterForm.ts
@@ -0,0 +1,42 @@
+import { useRegisterStore } from '..';
+import { useUserStore } from '../../user';
+import { RegisterFormData } from '../types';
+
+export const useRegisterForm = () => {
+  const {
+    tos,
+    univ,
+    setTos,
+    setUniv,
+    resetAll: resetRegister,
+  } = useRegisterStore();
+  const { authToken, userSocialType } = useUserStore();
+
+  const getFormData = (): RegisterFormData => {
+    return {
+      tos,
+      univ,
+      social: {
+        authToken: authToken || '',
+        provider: userSocialType || '',
+      },
+    };
+  };
+
+  const resetAll = () => {
+    resetRegister();
+  };
+
+  return {
+    tos,
+    univ,
+    authToken,
+    userSocialType,
+
+    setTos,
+    setUniv,
+
+    getFormData,
+    resetAll,
+  };
+};
