19166d23c2cc79b606397b3eebad7f4c3cbe336a
hyeone999 <caps026@naver.com>
2025-10-07 15:25:26 +0900

    ✨ Feat :  회원가입 완료 화면에서 홈화면 navigation 핸들링 구현

--- src/screens/Register/SignupSuccess.tsx
+++ src/screens/Register/SignupSuccess.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { useNavigation } from '@react-navigation/native';
+
 import WelcomeTextBox from '@/features/welcome/components/WelcomeTextBox';
+import { MainStackNavigationProp } from '@/navigation/types/navigationTypes';
 import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
 import HUButton from '@/shared/ui/atoms/HUButton';
 
 const SignupSuccessScreen = () => {
+  const navigation = useNavigation<MainStackNavigationProp>();
+
   return (
     <ScreenLayout className="items-center bg-primary-purple">
       <WelcomeTextBox
@@ -13,7 +18,11 @@ const SignupSuccessScreen = () => {
 커리어 성장하실 준비되셨나요?`}
       />
 
-      <HUButton variant="black" text="커리어 성장하러가기" />
+      <HUButton
+        variant="black"
+        text="커리어 성장하러가기"
+        onPress={() => navigation.navigate('HomeRoute')}
+      />
     </ScreenLayout>
   );
 };
