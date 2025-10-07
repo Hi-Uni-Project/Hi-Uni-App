df96009474f350c6573caf4551f41a5dbfb0a352
hyeone999 <caps026@naver.com>
2025-10-07 12:18:43 +0900

    ✨ Feat :  token 재발급 로직 구현

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -1,5 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import Config from 'react-native-config';
+
+import { useUserStore } from '../stores/user';
+
+import { Refresh, ResponseTypes } from './types';
 
 export const axiosInstance = axios.create({
   baseURL: Config.API_KEY,
@@ -9,65 +13,69 @@ export const axiosInstance = axios.create({
 });
 
 // accessToken Header interceptor
-// axiosInstance.interceptors.request.use(
-//   config => {
-//     const accessToken = useUserStore.getState().accessToken;
+axiosInstance.interceptors.request.use(
+  config => {
+    const accessToken = useUserStore.getState().accessToken;
 
-//     console.log(accessToken);
+    console.log(accessToken);
 
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
 
-//     console.log('axios config : ', config);
+    console.log('axios config : ', config);
 
-//     return config;
-//   },
+    return config;
+  },
 
-//   error => {
-//     console.log('axios config : ', error);
-//     return Promise.reject(error);
-//   },
-// );
+  error => {
+    console.log('axios config : ', error);
+    return Promise.reject(error);
+  },
+);
 
-// // accessToken 재발급 interceptor
-// axiosInstance.interceptors.response.use(
-//   response => response,
-//   async error => {
-//     const { refreshToken, setAccessToken, logout } = useUserStore.getState();
+// accessToken 재발급 interceptor
+axiosInstance.interceptors.response.use(
+  response => response,
+  async (error: AxiosError<ResponseTypes>) => {
+    const { refreshToken, setAccessToken, logout } = useUserStore.getState();
 
-//     // 리프레시 토큰 만료 → 로그아웃 처리 -> MainStack에서 관리
-//     if (error.response?.status === 1002) {
-//       logout();
-//       return Promise.reject(error);
-//     }
+    const code = error.response.data.responseCode;
 
-//     // 액세스 토큰 만료 → 재발급
-//     if (error.response?.status === 1001) {
-//       try {
-//         const res = await axiosInstance.post<ResponseTypes<Refresh>>(
-//           '/auth/refresh',
-//           {},
-//           {
-//             headers: {
-//               Authorization: `Bearer ${refreshToken}`,
-//             },
-//           },
-//         );
+    console.log('axios: ', code);
 
-//         const newAccessToken = res.data.data.accessToken;
+    // 리프레시 토큰 만료 → 로그아웃 처리 -> MainStack에서 관리
+    if (code === 1002) {
+      logout();
+      return Promise.reject(error);
+    }
 
-//         setAccessToken(newAccessToken);
-//         error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+    // 액세스 토큰 만료 → 재발급
+    if (code === 1001) {
+      try {
+        const res = await axios.post<ResponseTypes<Refresh>>(
+          `${Config.API_KEY}/auth/refresh`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          },
+        );
 
-//         return axiosInstance.request(error.config);
-//       } catch (refreshError) {
-//         // 재발급 실패 → 로그아웃 처리
-//         logout();
-//         return Promise.reject(refreshError);
-//       }
-//     }
+        const newAccessToken = res.data.data.accessToken;
 
-//     return Promise.reject(error);
-//   },
-// );
+        setAccessToken(newAccessToken);
+        error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axiosInstance.request(error.config);
+      } catch (refreshError) {
+        // 재발급 실패 → 로그아웃 처리
+        logout();
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
