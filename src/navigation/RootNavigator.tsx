dd8f0119a7fff51bdacbfc10f2850b68508d11c2
hyeone999 <caps026@naver.com>
2025-10-07 14:01:37 +0900

    ✨ Feat :  onboard route 새롭게 추가

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import SignupRoute from './stacks/SignupStackNavigator';
+import OnboardRoute from './stacks/OnBoardStackNavigation.';
 import HomeTabScreens from './tabs/TabNavigator';
 import { MainNavigationProps } from './types/navigationTypes';
-
-import LoginScreen from '@/screens/Login';
-import OnboardingScreen from '@/screens/Onboarding';
 
 const Stack = createNativeStackNavigator<MainNavigationProps>();
 
 const MainStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Onboarding" // 자동로그인 구현 시 -> Home으로 변경
+      initialRouteName="HomeRoute"
       screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="SignupRoute" component={SignupRoute} />
+      <Stack.Screen name="OnboardRoute" component={OnboardRoute} />
 
       <Stack.Screen name="HomeRoute" component={HomeTabScreens} />
     </Stack.Navigator>
