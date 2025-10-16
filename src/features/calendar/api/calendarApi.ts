b13681a855917553a90d43c04e48fab5b233f1da
seolbotdae <seolbotdae@gmail.com>
2025-10-16 15:03:03 +0900

    ✨ Feat :  캘린더 api 추가

---
+++ src/features/calendar/api/calendarApi.ts
@@ -0,0 +1,20 @@
+import { CalendarScheduleRequest, CalendarScheduleResponse } from '../types';
+
+import { axiosInstance } from '@/shared/api/axiosInstance';
+
+export const calendarScheduleApi = async ({
+  startDate,
+  endDate,
+}: CalendarScheduleRequest): Promise<CalendarScheduleResponse> => {
+  const response = await axiosInstance.get<CalendarScheduleResponse>(
+    '/schedules',
+    {
+      params: {
+        startDate,
+        endDate,
+      },
+    },
+  );
+
+  return response.data;
+};
