d5d585cf594cdf2c6548ab81c9a4a168a21502b7
hyeone999 <caps026@naver.com>
2025-10-20 12:46:51 +0900

    ✨ Feat :  homeStack에 hotBoard Screen 추가

--- src/navigation/stacks/HomeStackNavigator.tsx
+++ src/navigation/stacks/HomeStackNavigator.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { HomeNavigationProps } from '../types/navigationTypes';
+
 import HomeScreen from '@/screens/Home';
+import HotBoardScreen from '@/screens/Home/HotBoard';
 
 const DetailScreen = () => <>2</>;
 const ProfileScreen = () => <>3</>;
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<HomeNavigationProps>();
 
 const HomeRoute = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeMain" component={HomeScreen} />
+      <Stack.Screen name="HotBoard" component={HotBoardScreen} />
       <Stack.Screen name="Detail" component={DetailScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
