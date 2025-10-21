da26ca80d1b91e64029c00cdefd9269e1faba604
hyeone999 <caps026@naver.com>
2025-10-21 16:03:00 +0900

    ✨ Feat :  hot board screen 구현

--- src/screens/Home/HotBoard/index.tsx
+++ src/screens/Home/HotBoard/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { FlatList, View } from 'react-native';
+import { View } from 'react-native';
 
+import HotBoardContent from '@/features/home/hotBoard/components/HotBoardContent';
+import { useWeeklyHotPosts } from '@/features/home/hotBoard/hooks/useWeeklyHotPosts';
 import HotBoardHeader from '@/features/home/hotBoard/layouts/HotBoardHeader';
 import BoardHeaderColor from '@/features/home/shared/layouts/BoardHeaderColorGround';
-import BoardPostCardMD from '@/shared/components/BoardPostCard/md';
 import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
-import { mockBoardPosts } from '@/shared/constants/boardMockData';
 
 const HotBoardScreen = () => {
+  const { data: boardData, isLoading } = useWeeklyHotPosts();
+
   return (
     <View className="flex-1">
       <BoardHeaderColor />
@@ -16,16 +18,7 @@ const HotBoardScreen = () => {
 
       <ScreenLayout edges={['bottom']}>
         <View className="flex-1 px-5">
-          <FlatList
-            data={mockBoardPosts}
-            keyExtractor={(_, idx) => idx.toString()}
-            renderItem={({ item }) => <BoardPostCardMD {...item} />}
-            contentContainerStyle={{ paddingVertical: 20, gap: 8 }}
-            showsVerticalScrollIndicator={false}
-          />
-
-          {/* 주간 HOT 게시글이 없을 경우 */}
-          {/* <NoHotBoard /> */}
+          <HotBoardContent data={boardData} isLoading={isLoading} />
         </View>
       </ScreenLayout>
     </View>
