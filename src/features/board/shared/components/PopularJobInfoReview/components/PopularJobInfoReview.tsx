9f7b7544e89c23b8bf7239c7e4473b94d5ba2b70
hyeone999 <caps026@naver.com>
2025-10-28 15:24:18 +0900

    ✨ Feat :  shared/ 하위로 항목 이동

--- src/features/board/shared/components/PopularJobInfoReview/components/PopularJobInfoReview.tsx
+++ src/features/board/shared/components/PopularJobInfoReview/components/PopularJobInfoReview.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import { View, FlatList, Platform } from 'react-native';
 
-import BoardPostCardMocks from '@/features/board/mocks/BoardPostCardMocks';
-import SectionTitle from '@/features/board/shared/components/SectionTitle';
+import SectionTitle from '../../SectionTitle';
+
+import BoardPostCardMocks from '@/features/board/shared/mocks/BoardPostCardMocks';
 import BoardPostCardLG from '@/shared/components/Board/BoardPostCard/lg';
 
 interface Props {
