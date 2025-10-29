734224945ea8de548bf173962124eb0727817173
hyeone999 <caps026@naver.com>
2025-10-29 16:09:27 +0900

    ✨ Feat :  보드 메인 페이지 하단 영역 컴포넌트 구현

--- src/screens/Board/index.tsx
+++ src/screens/Board/index.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import JobInformationScreen from '@/features/board/boardMain/components/JobInformation';
 import BoardHeader from '@/features/board/shared/components/BoardHeader';
 import { TabKey } from '@/features/board/shared/types/BoardUpperTab';
+import SortPostList from '@/shared/components/Board/SortPostList';
+import { useSortBoard } from '@/shared/hooks/useSortBoard';
+import { mockPosts } from '@/shared/lib/mock';
 
 const BoardScreen = () => {
   const insets = useSafeAreaInsets();
   const [activeTab, setActiveTab] = useState<TabKey>('jobInfo');
+
+  const {
+    selectedSortLabel,
+    setSelectedSort,
+    sortSheetVisible,
+    setSortSheetVisible,
+  } = useSortBoard();
 
   const renderContent = () => {
     switch (activeTab) {
@@ -21,12 +31,22 @@ const BoardScreen = () => {
   };
 
   return (
-    <View className="relative">
+    <View className="flex-1">
       <BoardHeader onTabPress={setActiveTab} />
-      <ScrollView style={{ marginTop: insets.top + 110 }}>
-        {renderContent()}
-        <View className="h-[1000px]" />
-      </ScrollView>
+      <View style={{ marginTop: insets.top + 110 }}>{renderContent()}</View>
+
+      <View className="flex-1 px-4">
+        <SortPostList
+          classname=""
+          vertical
+          selectedSortLabel={selectedSortLabel}
+          setSelectedSort={setSelectedSort}
+          setSortSheetVisible={setSortSheetVisible}
+          sortSheetVisible={sortSheetVisible}
+          data={mockPosts}
+          onPostPress={() => console.log('ss')}
+        />
+      </View>
     </View>
   );
 };
