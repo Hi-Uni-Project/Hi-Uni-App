688a867f708c64fe58245ffdf3c07070487d4f35
hyeone999 <caps026@naver.com>
2025-10-31 14:08:54 +0900

    ✨ Feat :  카테고리 인기 후기 게시판 화면 구현

--- src/screens/Board/PopularReviews/index.tsx
+++ src/screens/Board/PopularReviews/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import BoardHeaderColorGround from '@/features/home/shared/layouts/BoardHeaderColorGround';
+import DetailBoardHeader from '@/shared/components/Board/layouts/DetailBoardHeader';
+import NoBoardLayout from '@/shared/components/Board/layouts/NoBoardLayout';
+import SortPostList from '@/shared/components/Board/SortPostList';
+import { useHideTabBar } from '@/shared/hooks/useHideTabBar';
+import { useSortBoard } from '@/shared/hooks/useSortBoard';
+import { mockPosts } from '@/shared/lib/mock';
+import Loading from '@/shared/ui/organisms/Loading';
 
 const PopularReviews = () => {
+  const isLoading = false;
+  useHideTabBar();
+
+  const {
+    selectedSortLabel,
+    setSelectedSort,
+    sortSheetVisible,
+    setSortSheetVisible,
+  } = useSortBoard();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!mockPosts || mockPosts.length === 0) {
+    return <NoBoardLayout des={'아직 취업 정보 인기글이'} />;
+  }
+
   return (
-    <View>
-      <Text>PopularReviews</Text>
+    <View className="flex-1">
+      <BoardHeaderColorGround />
+      <DetailBoardHeader title={'취업 정보 인기 후기'} icon={true} />
+
+      <View className="flex-1 bg-surface-50 px-5">
+        <SortPostList
+          classname=""
+          vertical
+          selectedSortLabel={selectedSortLabel}
+          setSelectedSort={setSelectedSort}
+          setSortSheetVisible={setSortSheetVisible}
+          sortSheetVisible={sortSheetVisible}
+          data={mockPosts}
+          onPostPress={() => console.log('ss')}
+        />
+      </View>
     </View>
   );
 };
