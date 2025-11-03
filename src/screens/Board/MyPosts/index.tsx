e0f136c7fd80641f586c709a6ecb28b8b685e534
hyeone999 <caps026@naver.com>
2025-11-03 11:25:11 +0900

    ✨ Feat :  화면과 연결된 query 수정

--- src/screens/Board/MyPosts/index.tsx
+++ src/screens/Board/MyPosts/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import BoardListScreen from '@/shared/components/Board/BoardList';
-import { useMyCommentsQuery } from '@/shared/hooks/useBoardQuery';
+import { useMyPostsQuery } from '@/shared/hooks/useBoardQuery';
 
 const MyPosts = () => {
   return (
     <BoardListScreen
       title="내가 쓴 글"
       emptyDescription="아직 작성한 글이"
-      useQuery={useMyCommentsQuery}
+      useQuery={useMyPostsQuery}
     />
   );
 };
