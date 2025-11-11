8f975a6f62c7de451b0114502b8928e77651270e
hyeone999 <caps026@naver.com>
2025-11-11 11:00:45 +0900

    ✨ Feat :  게시판-메인 인기글 구현

--- src/features/board/boardMain/components/PopularJobInfoReview.tsx
+++ src/features/board/boardMain/components/PopularJobInfoReview.tsx
@@ -1,52 +1,63 @@
 import React from 'react';
 
-import { View, FlatList, Platform } from 'react-native';
+import { View, FlatList, Platform, Pressable } from 'react-native';
 
 import SectionTitle from '../../shared/components/SectionTitle';
-import BoardPostCardMocks from '../../shared/mocks/BoardPostCardMocks';
+import {
+  getPostTypeByDisplayName,
+  PostCategory,
+} from '../../shared/types/enum/postEnum';
+import { useCategoryWeeklyHotQuery } from '../hooks/useWeeklyHotQuery';
 
 import BoardPostCardLG from '@/shared/components/Board/BoardPostCard/lg';
+import Loading from '@/shared/ui/organisms/Loading';
 
 interface Props {
-  onPress?: () => void;
   title: string;
+  onPress?: () => void;
 }
 
-const PopularJobInfoReview = ({ onPress, title }: Props) => {
-  const mockData = BoardPostCardMocks;
+const PopularJobInfoReview = ({ title, onPress }: Props) => {
+  const selectedPostType = getPostTypeByDisplayName(title);
+
+  const { data: posts = [], isLoading } = useCategoryWeeklyHotQuery(
+    PostCategory.JOB_INFORMATION,
+    selectedPostType,
+  );
+
+  if (isLoading) {
+    return (
+      <View className="bg-primary-purple pb-5 pt-[23px]">
+        <Loading />
+      </View>
+    );
+  }
 
   return (
-    <View className="bg-primary-purple pb-5 pt-[23px]">
-      <SectionTitle onPress={onPress} title={title} />
-      <FlatList
-        data={mockData}
-        keyExtractor={item => item.id}
-        nestedScrollEnabled={true}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={
-          Platform.OS === 'android' ? { paddingRight: 20 } : {}
-        }
-        className="flex-row px-5"
-        renderItem={({ item }) => (
-          <View className="mr-4" key={item.id}>
-            <BoardPostCardLG
-              company={item.company}
-              period={item.period}
-              position={item.position}
-              jobType={item.jobType}
-              title={item.title}
-              content={item.content}
-              author={item.author}
-              major={item.major}
-              time={item.time}
-              likes={item.likes}
-              comments={item.comments}
-            />
-          </View>
-        )}
-      />
-    </View>
+    <Pressable onPress={onPress}>
+      <View className="bg-primary-purple pb-5 pt-[23px]">
+        <SectionTitle title={`${title} 인기 후기`} onPress={onPress} />
+        <FlatList
+          data={posts}
+          keyExtractor={item => item.id.toString()}
+          nestedScrollEnabled={true}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={
+            Platform.OS === 'android' ? { paddingRight: 20 } : {}
+          }
+          className="flex-row px-5"
+          renderItem={({ item }) => (
+            <View className="mr-4">
+              <BoardPostCardLG
+                {...item}
+                onPress={() => console.log('Card clicked:', item.title)}
+              />
+            </View>
+          )}
+        />
+      </View>
+    </Pressable>
   );
 };
 
