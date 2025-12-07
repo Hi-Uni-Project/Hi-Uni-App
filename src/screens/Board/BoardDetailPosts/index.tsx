7a8788cf977d2a6b39bed1475cb555e442c20e0d
hyeone999 <caps026@naver.com>
2025-12-07 09:46:56 +0900

    ✨ Feat : 목 데이터 구조 수정 및 답글 mapping

--- src/screens/Board/BoardDetailPosts/index.tsx
+++ src/screens/Board/BoardDetailPosts/index.tsx
@@ -107,8 +107,19 @@ const BoardDetailPosts = () => {
       likes: 0,
       replies: [
         {
-          id: 3,
+          id: 4,
+          author: '익명3',
+          school: '산업디자인학과',
           content: '저도 부타드려여 ㅠㅠㅠ',
+          date: '6/23 22:08',
+          likes: 2,
+        },
+        {
+          id: 5,
+          author: '익명3',
+          school: '산업디자인학과',
+          content:
+            '저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ저도 부타드려여 ㅠㅠㅠ',
           date: '6/23 22:08',
         },
       ],
@@ -305,74 +316,75 @@ const BoardDetailPosts = () => {
                     </View>
                   </View>
 
-                  {commentItem.replies && (
-                    <View className="flex-row space-x-2 px-5 pl-2 pt-4">
-                      <CommentArrowIcon className="top-2" />
+                  {commentItem.replies &&
+                    commentItem.replies.map(reply => (
+                      <View className="flex-row space-x-2 px-5 pl-2 pt-4">
+                        <CommentArrowIcon className="top-2" />
 
-                      <View className="w-full flex-col rounded-[10px] bg-surface-100 p-3.5">
-                        <View className="mb-3 flex-row justify-between">
-                          <View className="flex-row items-center">
-                            <View className="mr-3 h-8 w-8 rounded-full bg-surface-300" />
-                            <Text className="mr-2 text-main-text typo-caption-14-semibold">
-                              {commentItem.author}
-                            </Text>
-                            <Text className="text-surface-500 typo-caption-14-regular">
-                              · {commentItem.school}
-                            </Text>
-                          </View>
-
-                          <View className="flex-row items-center space-x-2 rounded-[100px] bg-surface-200 px-3 py-2">
-                            <Pressable>
-                              <CommentActionIcons
-                                action="like"
-                                height={18}
-                                width={18}
-                              />
-                            </Pressable>
-
-                            <View className="h-3 w-[1px] bg-surface-300" />
-
-                            <Pressable
-                              onPress={() =>
-                                setActiveCommentOption(
-                                  activeCommentOption === commentItem.id
-                                    ? null
-                                    : commentItem.id,
-                                )
-                              }>
-                              <CommentActionIcons
-                                action="toggle"
-                                height={18}
-                                width={18}
-                              />
-                            </Pressable>
-                          </View>
-                        </View>
-
-                        <Text className="mb-2 text-main-text typo-body-15-regular">
-                          {commentItem.content}
-                        </Text>
-
-                        <View className="flex-row items-center space-x-2">
-                          <Text className="text-surface-500 typo-caption-13-light">
-                            {commentItem.date}
-                          </Text>
-                          {commentItem.likes > 0 && (
+                        <View className="w-full flex-col rounded-[10px] bg-surface-100 p-3.5">
+                          <View className="mb-3 flex-row justify-between">
                             <View className="flex-row items-center">
-                              <BoardActionIcons
-                                width={14}
-                                height={14}
-                                action="like"
-                              />
-                              <Text className="ml-1 text-xs text-red-500">
-                                {commentItem.likes}
+                              <View className="mr-3 h-8 w-8 rounded-full bg-surface-300" />
+                              <Text className="mr-2 text-main-text typo-caption-14-semibold">
+                                {commentItem.author}
+                              </Text>
+                              <Text className="text-surface-500 typo-caption-14-regular">
+                                · {commentItem.school}
                               </Text>
                             </View>
-                          )}
+
+                            <View className="flex-row items-center space-x-2 rounded-[100px] bg-surface-200 px-3 py-2">
+                              <Pressable>
+                                <CommentActionIcons
+                                  action="like"
+                                  height={18}
+                                  width={18}
+                                />
+                              </Pressable>
+
+                              <View className="h-3 w-[1px] bg-surface-300" />
+
+                              <Pressable
+                                onPress={() =>
+                                  setActiveCommentOption(
+                                    activeCommentOption === commentItem.id
+                                      ? null
+                                      : commentItem.id,
+                                  )
+                                }>
+                                <CommentActionIcons
+                                  action="toggle"
+                                  height={18}
+                                  width={18}
+                                />
+                              </Pressable>
+                            </View>
+                          </View>
+
+                          <Text className="mb-2 text-main-text typo-body-15-regular">
+                            {reply.content}
+                          </Text>
+
+                          <View className="flex-row items-center space-x-2">
+                            <Text className="text-surface-500 typo-caption-13-light">
+                              {reply.date}
+                            </Text>
+                            {reply.likes > 0 && (
+                              <View className="flex-row items-center">
+                                <BoardActionIcons
+                                  width={14}
+                                  height={14}
+                                  action="like"
+                                />
+                                <Text className="ml-1 text-xs text-red-500">
+                                  {reply.likes}
+                                </Text>
+                              </View>
+                            )}
+                          </View>
                         </View>
                       </View>
-                    </View>
-                  )}
+                    ))}
                 </View>
 
                 {/* 댓글 옵션 팝업 */}
