5c6f3cb0979e6be1e8ea359c87e905e948e527eb
hyeone999 <caps026@naver.com>
2025-12-07 10:53:37 +0900

    ✨ Feat : 액션 아이콘 추가

--- src/shared/icons/CommentActionIcons.tsx
+++ src/shared/icons/CommentActionIcons.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import CommentsIcon from '@/static/icons/comment-box.svg';
+import SendOnIcon from '@/static/icons/comment-send-on.svg';
+import SendIcon from '@/static/icons/comment-send.svg';
 import ThumbsUpIcon from '@/static/icons/like-box.svg';
 import ToggleIcon from '@/static/icons/toggle-box.svg';
 
 interface Props {
-  action: 'like' | 'comment' | 'toggle';
+  action: 'like' | 'comment' | 'toggle' | 'send' | 'send-on';
   width?: number;
   height?: number;
 }
@@ -18,6 +20,10 @@ const CommentActionIcons = ({ action, height, width }: Props) => {
       return <CommentsIcon width={width} height={height} />;
     case 'toggle':
       return <ToggleIcon width={width} height={height} />;
+    case 'send':
+      return <SendIcon width={width} height={height} />;
+    case 'send-on':
+      return <SendOnIcon width={width} height={height} />;
     default:
       return null;
   }
