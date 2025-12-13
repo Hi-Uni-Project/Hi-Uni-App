47ed09bc2234f6d993fe8dd89afbe05d1be15702
hyeone999 <caps026@naver.com>
2025-12-13 10:35:34 +0900

    ✨ Feat : 개발 중단된 기능에 대한 코멘트 처리

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Animated, { LinearTransition } from 'react-native-reanimated';
 
-import DynamicCTABanner from '@/features/home/dynamicCTABanner/components/DynamicCTABanner';
-import NextUpdateTemplate from '@/features/home/nextUpdateTemplate/components/NextUpdateTemplate';
 import ScheduleWidget from '@/features/home/scheduleWidget/components/ScheduleWidget';
 import HomeScrollLayout from '@/features/home/shared/layouts/HomeScrollLayout';
 import UnivMajorBadge from '@/features/home/univMajorBadge/components/UnivMajorBadge';
@@ -16,22 +14,24 @@ const HomeScreen = () => {
   return (
     <HomeScrollLayout>
       <UnivMajorBadge univ={univ} />
-      <DynamicCTABanner
+      {/* 개발 중간 기능 -> 확인 필요 */}
+      {/* <DynamicCTABanner
         visible
         onSharedPress={() => {
           console.log('공유 페이지 네비게이션');
         }}
-      />
+      /> */}
       <ScheduleWidget />
       <HotPostsWidget />
       <Animated.View
         style={{ rowGap: 38, marginTop: 38, marginBottom: 20 }}
         layout={LinearTransition}>
-        <NextUpdateTemplate title="대외활동" />
+        {/* 개발 중간 기능 -> 확인 필요 */}
+        {/* <NextUpdateTemplate title="대외활동" />
 
         <NextUpdateTemplate title="인턴십" />
 
-        <NextUpdateTemplate title="채용공고" />
+        <NextUpdateTemplate title="채용공고" /> */}
       </Animated.View>
     </HomeScrollLayout>
   );
