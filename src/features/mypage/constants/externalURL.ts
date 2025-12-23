e030cd92adf1905d6a4d54763b7da612ae38fb9b
hyeone999 <caps026@naver.com>
2025-12-23 10:30:42 +0900

    ✨ Feat : 마이페이지 링킹 구현

---
+++ src/features/mypage/constants/externalURL.ts
@@ -0,0 +1,8 @@
+export const EXTERNAL_URLS = {
+  /** 서비스 이용약관 */
+  TERMS: 'https://www.notion.so/2cc73bb1e6c580c0a6cec6c9da239c83',
+  /** 커뮤니티 이용규칙 */
+  RULES: 'https://www.notion.so/2cc73bb1e6c580808964deb8a8578f1b',
+  /** 개인정보 처리방침 */
+  PRIVACY: 'https://www.notion.so/2cc73bb1e6c58030b173c66342155ee2',
+} as const;
