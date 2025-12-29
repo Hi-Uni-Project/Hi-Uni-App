b40f885aa8408f77354684c8f09b55250aa3e4df
hyeone999 <caps026@naver.com>
2025-12-29 13:52:53 +0900

    ✨ Feat : 최근 검색어 android에서 3줄 째 border가 안보이는 이슈 수정

--- src/features/home/searchBoard/components/RecentSearchList.tsx
+++ src/features/home/searchBoard/components/RecentSearchList.tsx
@@ -18,31 +18,31 @@ const RecentSearchList = ({
   handleSelectItem,
 }: Props) => {
   return (
-    <View
-      className="flex-row flex-wrap gap-2 gap-y-3 overflow-hidden pt-1"
-      style={{ maxHeight: 4 * 42 }}>
-      {recentSearches.length === 0 ? (
-        <NoRecent />
-      ) : (
-        recentSearches.map(item => (
-          <Pressable
-            onPress={() => handleSelectItem(item)}
-            key={item}
-            className="flex-row items-center rounded-[20px] border border-surface-300 px-4 py-2">
-            <Text className="mr-2 text-surface-600 typo-body-16-regular">
-              {item}
-            </Text>
-            <Pressable onPress={() => handleRemoveItem(item)}>
-              <ActionIcons
-                type="close"
-                height={12}
-                width={12}
-                color="#6E6E6E"
-              />
+    <View style={{ maxHeight: 3 * 42 + 2 * 12 + 4, overflow: 'hidden' }}>
+      <View className="flex-row flex-wrap gap-2 gap-y-3 pb-1 pt-1">
+        {recentSearches.length === 0 ? (
+          <NoRecent />
+        ) : (
+          recentSearches.map(item => (
+            <Pressable
+              onPress={() => handleSelectItem(item)}
+              key={item}
+              className="flex-row items-center rounded-[20px] border border-surface-300 px-4 py-2">
+              <Text className="mr-2 text-surface-600 typo-body-16-regular">
+                {item}
+              </Text>
+              <Pressable onPress={() => handleRemoveItem(item)}>
+                <ActionIcons
+                  type="close"
+                  height={12}
+                  width={12}
+                  color="#6E6E6E"
+                />
+              </Pressable>
             </Pressable>
-          </Pressable>
-        ))
-      )}
+          ))
+        )}
+      </View>
     </View>
   );
 };
