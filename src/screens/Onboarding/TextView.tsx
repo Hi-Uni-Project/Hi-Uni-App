b24a88a4aaefd792ebe93e9447c5fe20bada4f93
hyeone999 <caps026@naver.com>
2025-12-29 09:12:30 +0900

    ✨ Feat : 온보딩 화면 디자인 적용 및 라이팅 문구 수정

--- src/screens/Onboarding/TextView.tsx
+++ src/screens/Onboarding/TextView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { ImageSourcePropType, View } from 'react-native';
 import Animated, { FadeOutUp, FadeInDown } from 'react-native-reanimated';
 
 import ONBOARDING_PROVIDER from '../../shared/constants/onboardingProvider';
@@ -11,6 +11,14 @@ interface Props {
 
 const TextView = ({ currentStep }: Props) => {
   const onboardingProvider = ONBOARDING_PROVIDER;
+
+  const onboardingImages: Record<number, ImageSourcePropType> = {
+    0: require('@/assets/onboarding/onboarding-1.png'),
+    1: require('@/assets/onboarding/onboarding-2.png'),
+    2: require('@/assets/onboarding/onboarding-3.png'),
+    3: require('@/assets/onboarding/onboarding-4.png'),
+  };
+
   return (
     <View key={`TextView-${currentStep}`} className="flex-1">
       <View className="m-5 mt-28">
@@ -39,6 +47,18 @@ const TextView = ({ currentStep }: Props) => {
               {line}
             </Animated.Text>
           ))}
+      </View>
+
+      <View className="mb-24 flex-1 items-center justify-center">
+        <Animated.Image
+          key={`TextView-${currentStep}-Image`}
+          entering={FadeInDown.delay(300).duration(800)}
+          exiting={FadeOutUp.duration(300)}
+          className="h-[450px] w-[340px]"
+          source={onboardingImages[currentStep]}
+          width={1}
+          height={1}
+        />
       </View>
     </View>
   );
