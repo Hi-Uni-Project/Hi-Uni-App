9ea33916420d38e4b27ba3a9072237478b935ed6
hyeone999 <caps026@naver.com>
2025-12-30 10:17:51 +0900

    ✨ Feat : 본인이 작성한 글일 경우 좋아요/스크랩 안보이게끔 수정

--- src/features/board/boardDetail/components/PostStats.tsx
+++ src/features/board/boardDetail/components/PostStats.tsx
@@ -10,6 +10,7 @@ interface Props {
   likes: number;
   bookmarks: number;
   isLiked: boolean;
+  isUser: boolean;
   isBookmarked: boolean;
   likeScale: Animated.Value;
   bookmarkScale: Animated.Value;
@@ -22,6 +23,7 @@ const PostDetailStats = ({
   likes,
   bookmarks,
   isLiked,
+  isUser,
   isBookmarked,
   likeScale,
   bookmarkScale,
@@ -30,8 +32,10 @@ const PostDetailStats = ({
 }: Props) => {
   return (
     <View className="flex-row items-center justify-between space-x-6 py-6">
-      <View className="flex-row items-center">
-        {/* <ToggleIcons
+      {!isUser && (
+        <>
+          <View className="flex-row items-center">
+            {/* <ToggleIcons
           type="eyeOpenStroke"
           color="#979797"
           width={24}
@@ -40,58 +44,62 @@ const PostDetailStats = ({
         <Text className="ml-1 text-surface-500 typo-body-15-medium">
           {views}명이 봤어요
         </Text> */}
-        {/* 조회수 데이터 이슈로 인한 코멘트처리 */}
-      </View>
+            {/* 조회수 데이터 이슈로 인한 코멘트처리 */}
+          </View>
 
-      <View className="flex-row gap-2">
-        <Pressable
-          onPress={onLikePress}
-          className={clsx(
-            'flex-row items-center rounded-[50px] border-[1.5px] px-3.5 py-1.5',
-            isLiked ? 'border-main-red bg-main-red/10' : 'border-surface-200',
-          )}
-          style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }]}>
-          <Animated.View style={{ transform: [{ scale: likeScale }] }}>
-            <BoardActionIcons
-              action={isLiked ? 'like-fill' : 'like-gray'}
-              width={20}
-              height={20}
-            />
-          </Animated.View>
-          <Text
-            className={clsx(
-              'ml-1 min-w-[18px] text-right typo-caption-14-semibold',
-              isLiked ? 'text-main-red' : 'text-surface-600',
-            )}>
-            {likes}
-          </Text>
-        </Pressable>
+          <View className="flex-row gap-2">
+            <Pressable
+              onPress={onLikePress}
+              className={clsx(
+                'flex-row items-center rounded-[50px] border-[1.5px] px-3.5 py-1.5',
+                isLiked
+                  ? 'border-main-red bg-main-red/10'
+                  : 'border-surface-200',
+              )}
+              style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }]}>
+              <Animated.View style={{ transform: [{ scale: likeScale }] }}>
+                <BoardActionIcons
+                  action={isLiked ? 'like-fill' : 'like-gray'}
+                  width={20}
+                  height={20}
+                />
+              </Animated.View>
+              <Text
+                className={clsx(
+                  'ml-1 min-w-[18px] text-right typo-caption-14-semibold',
+                  isLiked ? 'text-main-red' : 'text-surface-600',
+                )}>
+                {likes}
+              </Text>
+            </Pressable>
 
-        <Pressable
-          onPress={onBookmarkPress}
-          className={clsx(
-            'flex-row items-center rounded-[50px] border-[1.5px] px-3.5 py-1.5',
-            isBookmarked
-              ? 'border-tertiary-yellow bg-tertiary-yellow/[7%]'
-              : 'border-surface-200',
-          )}
-          style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }]}>
-          <Animated.View style={{ transform: [{ scale: bookmarkScale }] }}>
-            <BoardActionIcons
-              action={isBookmarked ? 'scrab-on' : 'scrab-gray'}
-              width={20}
-              height={20}
-            />
-          </Animated.View>
-          <Text
-            className={clsx(
-              'ml-1 min-w-[18px] text-right typo-caption-14-semibold',
-              isBookmarked ? 'text-tertiary-yellow' : 'text-surface-600',
-            )}>
-            {bookmarks}
-          </Text>
-        </Pressable>
-      </View>
+            <Pressable
+              onPress={onBookmarkPress}
+              className={clsx(
+                'flex-row items-center rounded-[50px] border-[1.5px] px-3.5 py-1.5',
+                isBookmarked
+                  ? 'border-tertiary-yellow bg-tertiary-yellow/[7%]'
+                  : 'border-surface-200',
+              )}
+              style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }]}>
+              <Animated.View style={{ transform: [{ scale: bookmarkScale }] }}>
+                <BoardActionIcons
+                  action={isBookmarked ? 'scrab-on' : 'scrab-gray'}
+                  width={20}
+                  height={20}
+                />
+              </Animated.View>
+              <Text
+                className={clsx(
+                  'ml-1 min-w-[18px] text-right typo-caption-14-semibold',
+                  isBookmarked ? 'text-tertiary-yellow' : 'text-surface-600',
+                )}>
+                {bookmarks}
+              </Text>
+            </Pressable>
+          </View>
+        </>
+      )}
     </View>
   );
 };
